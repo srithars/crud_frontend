c95e90e93164e88791c5670e9724550b42eb41aa
srithars <64460687+srithars@users.noreply.github.com>
2025-07-08 06:17:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ const BASE_URL = 'http://localhost:9090/springapp1';
     
   const deleteProduct = async (id) => {
     if (window.confirm('Are you sure you want to delete this product?')) {
-      await axios.delete(`${BASE_URL}/${id}`);
+      await axios.delete(`${BASE_URL}/delete/${id}`);
       alert('Delete successful');
       fetchProducts();
     }
